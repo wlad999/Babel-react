d3b25ae3ad30cd1a79c1b4855a22df383bba1bd2
wlad999 <wlad25888@gmail.com>
2020-09-10 18:30:07 +0300

    159 Babel presets

---
+++ build/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var App = function App() {
+  _classCallCheck(this, App);
+
+  _defineProperty(this, "run", function () {
+    var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "World";
+    console.log("Hello ".concat(name));
+  });
+};
+
+var app = new App();
+app.run();
