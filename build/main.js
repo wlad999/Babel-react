7ead6042d0b195d5587ef53a4a7427f0fb63ef18
wlad999 <wlad25888@gmail.com>
2020-09-10 19:18:59 +0300

    161.1 dynamic browser selection "> 0.3%","not ie > 0"

--- build/main.js
+++ build/main.js
@@ -1,15 +1,25 @@
 "use strict";
 
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 class App {
   constructor() {
-    _defineProperty(this, "run", async (name = "World") => {
-      console.log(`Hello ${name}`);
-    });
+    _defineProperty(this, "run", /*#__PURE__*/function () {
+      var _ref = _asyncToGenerator(function* (name = "World") {
+        console.log("Hello ".concat(name));
+      });
+
+      return function () {
+        return _ref.apply(this, arguments);
+      };
+    }());
   }
 
 }
 
-const app = new App();
+var app = new App();
 app.run().then(() => console.log("DONE")).catch(() => console.log("ERROR!")); //npx babel src --out-dir build
